26af38a4a65fc704cc812137f06971ced00f8da6
jackr276 <jack_m_robbins@me.com>
2024-03-27 11:50:56 -0400

    basic functionality achieved

--- src/examine.c
+++ src/examine.c
@@ -4,12 +4,15 @@
  * Homework 06 - only problem(argv memory content examiner)
  */
 
-#include <stdlib.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 
 int main(int argc, char** argv){
+	//Predeclare prototypes of all needed functions
 	void dumpPtrContents(void* ptr);
+	void dumpStringContents(char* arg, int argLength);
 
 	//First print out the argv pointer
 	printf("\nargv    | ");
@@ -22,10 +25,24 @@ int main(int argc, char** argv){
 		printf("\n");
 	}
 
+	//Get the total length of all arguments, the dumpStringContents function needs this to know
+	//exactly how much to print out
+	int argLength = 0;
+	for(int i = 0; i < argc; i++){
+		//Add the length of the string plus 1 for the null character
+		argLength += strlen(argv[i]) + 1;
+	}
+
+	//Call the dumpStringContents function to handle printing the arguments
+	dumpStringContents(*argv, argLength);
+
 	return 0;
 }
 
 
+/**
+ * Prints out the contents(which are an address) contained in the pointer ptr
+ */
 void dumpPtrContents(void* ptr){	
 	//cast to unsigned char* to allow for dereferencing	
 	unsigned char* ptrCpy = (unsigned char*)ptr;
@@ -34,7 +51,39 @@ void dumpPtrContents(void* ptr){
 	for(int i = 7; i > -1; i--){
 		printf("%02hhx ", *(ptrCpy + i));
 	}
-	
+	//print out the address of ptr at the end	
 	printf("| %p\n", ptr);
+}
+
+void dumpStringContents(char* arg, int argLength){
+	//perform initial adjustment to make sure arg is 8-Byte aligned
+	unsigned short nibble = (long long)arg & 0xF;
+	//Push to the last 8-byte aligned address
+	arg -= (nibble % 8);
+
+	for(int sum = 0; sum <= argLength + argLength % 8; sum+=8){
+		printf("|");
+		
+		for(int j = 7; j > -1; j--){
+			unsigned char BYTE = *(arg + j);
+			//Print out hexadecimal version of BYTE
+			printf("%02hhx(", BYTE);
+
+			//If byte is char printable, print the char. Otherwise print int version
+			if(isprint(BYTE)){
+				printf("%c", BYTE);
+			} else {
+				printf("\\%d", BYTE);
+			}
+
+			printf(") ");
+		}
+	
+	//Print out the address of arg at the very end
+	printf(" | %p\n\n", arg);
+
+	//increment arg by 8
+	arg+=8;
+	}
 }
 
